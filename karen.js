2cc1c82bde1508d594c70a44049ceae6164200a5
MatheusLucasAndrade <theuslucasandrade@gmail.com>
2023-01-13 22:52:21 -0300

    aula 1

---
+++ karen.js
@@ -0,0 +1,24 @@
+/*
+    ATIVIDADE KAREN.
+
+    1 - Um array simples                []
+      - Um array bidimensional
+      - Um array tridimensional
+      (Console.log de cada array criado)
+
+    2 - Um objeto simples               {}
+      - Um Objeto de objeto
+      - Um Objeto de Objeto com Array
+
+    3 - Crie um sistema simples que verifica se o valor da variável
+        é igual a 10, se verdadeiro, some 40 há essa mesma variável
+        output: 50;
+      
+      - Crie um sistema simples que verifica se 3 valores de variáveis
+        são IDÊNTICAS, se verdadeiro retornar: output: "São idênticas"
+      
+      - Crie um sistema simples que verifica se pelo menos 1 valor de variáveis
+        são IDÊNTICAS, se verdadeiro retornar: output: "Condição verdadeira"
+
+      - Crie um sistema que NEGUE o valor de um interruptor de tomada
+*/
